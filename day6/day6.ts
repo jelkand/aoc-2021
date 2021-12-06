92e22f7992725026e9da7eba9cfb416bbcee8a27
Jack Anderson <jack@jackanderson.dev>
2021-12-06 13:42:38 -0500

    clean up day6 but no refactor

--- day6/day6.ts
+++ day6/day6.ts
@@ -1,51 +1,38 @@
-// const day6Input = (await Deno.readTextFile("./sample-input"))
-//   .split(",")
-//   .map(Number);
+const day6Input = (await Deno.readTextFile("./day6-input"))
+  .split(",")
+  .map(Number);
 
-// const DAYS = 256;
-// const SPOOL = 2; // time from spawning to when a fish will start the regular spawn cycle
-// const SPAWN_CYCLE = 7;
+const DAYS = 256;
+const SPAWN_CYCLE = 7;
+const SPOOL = 2;
 
-// const day6Part1 = (day6Input: number[]) =>
-//   day6Input.length + sum(day6Input.map(countChildren));
+const groupInput = (input: number[]) =>
+  input.reduce((acc, val) => {
+    acc[val] ||= 0;
+    acc[val] += 1;
+    return acc;
+  }, {} as Record<string, number>);
 
-// const sum = (args: number[]) => args.reduce((a, b) => a + b, 0);
+const day6 = (day6Input: number[]) => {
+  const grouped = groupInput(day6Input);
 
-// const spawnChildren = (spawnStartDate: number): number[] => {
-//   // note the day at which a fish starts spawning is not the day at which it is spawned
-//   if (spawnStartDate + SPAWN_CYCLE > DAYS) return [];
-//   return [
-//     spawnStartDate + SPAWN_CYCLE,
-//     ...spawnChildren(spawnStartDate + SPAWN_CYCLE),
-//   ];
-// };
+  let iteration = grouped;
 
-// const spawnChildrenIterative = (spawnStartDate: number): number[] => {
-//   let lastSpawnDate = spawnStartDate;
-//   const spawns = [];
+  for (let iter = 0; iter < DAYS; iter++) {
+    const entries = Object.entries(grouped);
+    iteration = entries.reduce((acc, [cycleVal, count]) => {
+      if (iter % SPAWN_CYCLE === +cycleVal % SPAWN_CYCLE && iter >= +cycleVal) {
+        acc[+iter + SPAWN_CYCLE + SPOOL] ||= 0;
+        acc[+iter + SPAWN_CYCLE + SPOOL] += count;
+      }
+      return acc;
+    }, iteration);
+  }
 
-//   while (lastSpawnDate < DAYS) {
-//     lastSpawnDate += SPAWN_CYCLE;
-//     spawns.push(lastSpawnDate);
-//   }
+  return Object.values(iteration).reduce((a, b) => a + b);
+};
 
-//   return spawns;
-// };
-
-// // console.log(spawnChildren(1), spawnChildrenIterative(1));
-
-// const countChildren = (spawnStartDate: number): number => {
-//   if (spawnStartDate > DAYS) {
-//     return 0;
-//   }
-
-//   // increment children to the point where they are on a spawn cycle
-//   const childrenStarts = spawnChildren(spawnStartDate).map((s) => s + SPOOL);
-
-//   return childrenStarts.length + sum(childrenStarts.map(countChildren));
-// };
-
-// const day6p1 = day6Part1(day6Input);
-// console.log({ day6p1 });
-
-// // console.log(spawnChildren(9));
+console.time("solve");
+const result = day6(day6Input);
+console.log({ result });
+console.timeEnd("solve");
