5de2d4db17b5e727f41104076350818e9dee606c
Jack Anderson <jack@jackanderson.dev>
2021-12-14 14:29:19 -0500

    finish and run for real input

--- day14/day14.ts
+++ day14/day14.ts
@@ -1,7 +1,7 @@
 import { slidingWindows } from "https://deno.land/std@0.117.0/collections/mod.ts";
 
 export const [rawInitial, rawRules] = (
-  await Deno.readTextFile("./sample-input")
+  await Deno.readTextFile("./day14-input")
 ).split("\n\n");
 
 const initial = rawInitial.split("");
@@ -18,26 +18,6 @@ const rules = rawRules.split("\n").reduce((acc, rule) => {
     },
   };
 }, {} as RuleMap);
-
-const tickTemplate = (template: string[]): string[] => {
-  if (template.length === 1) return [template[0]];
-  const [head, ...rest] = template;
-  const toInsert = rules[head][rest[0]];
-  return [head, toInsert, ...tickTemplate(rest)];
-};
-
-//memoizable
-const tickTemplateDeep = (pair: string[], depth: number): string[] => {
-  const [first, last] = pair;
-  const middle = rules[first][last];
-  if (depth === TICKS) return [first, middle, last];
-
-  const initial = tickTemplateDeep([first, middle], depth + 1);
-  const [_, ...final] = tickTemplateDeep([middle, last], depth + 1);
-  return [...initial, ...final];
-};
-
-const cache: Record<string, string[]> = {};
 
 const countCache: Record<string, Record<string, number>> = {};
 
@@ -58,21 +38,6 @@ const countKeys = (strArray: string[]) =>
     {} as Record<string, number>
   );
 
-const tickTemplateMemo = (pair: string[], depth: number): string[] => {
-  const cacheKey = [...pair, depth].join("");
-  if (cache[cacheKey]) return cache[cacheKey];
-
-  const [first, last] = pair;
-  const middle = rules[first][last];
-  if (depth === 1) return [first, middle, last];
-
-  const initial = tickTemplateMemo([first, middle], depth - 1);
-  const [_, ...final] = tickTemplateMemo([middle, last], depth - 1);
-  const result = [...initial, ...final];
-  cache[cacheKey] = result;
-  return result;
-};
-
 const countRecursive = (
   pair: string[],
   depth: number
@@ -90,14 +55,21 @@ const countRecursive = (
   return result;
 };
 
-const TICKS = 10;
+const TICKS = 40;
 
 const day14 = (input: string[]) => {
   const windows = slidingWindows(input, 2);
-  const result = countRecursive(windows[0], TICKS);
+  const result = windows.reduce((acc, window) => {
+    const wCounts = countRecursive(window, TICKS);
+    wCounts[window[1]] -= 1; // remove double counting for window overlap
+    return mergeObjects(acc, wCounts);
+  }, {} as Record<string, number>);
+
+  result[windows[windows.length - 1][1]] += 1; // don't remove the final letter in the above dobule counting squad
   const sorted = Object.entries(result).sort((a, b) => a[1] - b[1]);
+
   return sorted[sorted.length - 1][1] - sorted[0][1];
 };
 
-const part1 = day14(initial);
-console.log({ part1 });
+const result = day14(initial);
+console.log({ result });
